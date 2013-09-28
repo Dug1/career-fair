0b5a75fbbb9c9843766d1a94c31ea8e6f943bd08
Daguan <daguanlu@gmail.com>
2013-09-28 15:29:37 -0700

    update constants

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import pygame, sys, os
+
+from random import choice
 from pygame.local import *
 from pygame.image import load
 
 SCREEN_DIM = (700, 800)
-
-floor_tile = pygame.image.load("")
+person_limit = 30
+tile_size = 16 
 
 #loading image files
-
 
 os.chdir("images")
 persons_images = [(load(directory + "/front.png"), directory + load("/back.png"),directory + load("/side.png")) for directory in os.listdir if "person" in directory]
@@ -18,6 +19,10 @@ surface =  pygame.display.set_mode(SCREEN_DIM)
 clock = pygame.time.Clock()
 floor = Floor(SCREEN_DIM)
 
+#make stands 
+
+#generate random people
+
 while True: 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -26,5 +31,7 @@ while True:
     time_passed = clock.tick(30)
 
     #reapply the background
+    floor.flash_background(surface)
+
 
 
