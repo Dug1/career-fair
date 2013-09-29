c5d3ecd5abd437561c34505773c374b66e9de000
Daguan <daguanlu@gmail.com>
2013-09-28 20:21:32 -0700

    Add hero

--- main.py
+++ main.py
@@ -21,24 +21,25 @@ floor = Floor(SCREEN_DIM)
 
 #make stands 
 def makeStands():
-	import pygame
-	from pygame import Rect
-	tilesize = 16
-	width = 2 * tilesize
-	height = 3 * tilesize
-	intermittentspace = 10 * tilesize
-	vspace = 5 * tilesize
-	a = []
-	x = intermittentspace
-	while x <= (width * 3 + intermittentspace * 3):																	#True is left, False is right
-		for y in range(vspace + height, vspace + 5 * height + 1, height):
-			a.append(Stand(Rect((x, y), (width, height)), True))
-		x += width
-		for y in range(vspace + height, vspace + 5 * height + 1, height):
-			a.append(Stand(Rect((x, y), (width, height)), False))
-		x += intermittentspace
-	return a
-	
+    import pygame
+    from pygame import Rect
+    tilesize = 16
+    width = 2 * tilesize
+    height = 3 * tilesize
+    intermittentspace = 10 * tilesize
+    vspace = 5 * tilesize
+    a = []
+    x = intermittentspace
+    while x <= (width * 3 + intermittentspace * 3):
+        #True is left, False is right
+        for y in range(vspace + height, vspace + 5 * height + 1, height):
+            a.append(Stand(Rect((x, y), (width, height)), True))
+        x += width
+        for y in range(vspace + height, vspace + 5 * height + 1, height):
+            a.append(Stand(Rect((x, y), (width, height)), False))
+        x += intermittentspace
+    return a
+    
 #generate random people
 
 while True: 
